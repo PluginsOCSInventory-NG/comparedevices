c68caa60ab6a7e60bc01497f89f86368cef4be18
Lea Droguet <ljdroguet@gmail.com>
2020-11-03 15:58:52 +0000

    update ms compare

--- ms_compare_xmls/ms_compare_xmls.php
+++ ms_compare_xmls/ms_compare_xmls.php
@@ -1,74 +1,222 @@
 <?php
-if(AJAX){
+/*
+ * Copyright 2005-2016 OCSInventory-NG/OCSInventory-ocsreports contributors.
+ * See the Contributors file for more details about them.
+ *
+ * This file is part of OCSInventory-NG/OCSInventory-ocsreports.
+ *
+ * OCSInventory-NG/OCSInventory-ocsreports is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * OCSInventory-NG/OCSInventory-ocsreports is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OCSInventory-NG/OCSInventory-ocsreports. if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+if (AJAX) {
     parse_str($protectedPost['ocs']['0'], $params);
-    $protectedPost+=$params;
+    $protectedPost += $params;
     ob_start();
-    $ajax = true;
 }
-else{
-    $ajax=false;
-}
-
-echo "<link rel='stylesheet' href='/style.css'>";
-
-include_once('Difference_Class.php');
-include_once('ArrayFromXML_Class.php');
-include_once('Table_Class.php');
-
-/* 
-User chooses main device to compare against first then every other device(s) to 
-compare with
-First value of xmls array is considered main device
-*/
+require_once('require/function_computers.php');
+require_once('require/function_admininfo.php');
+//intégration des fonctions liées à la recherche multicritère
+require_once('require/function_search.php');
 
 
-// --------------------------------------------- TEST -------------------------------------------
-// array of xmls files
-$xmls = array("xmls_tests/test.xml", "xmls_tests/test2.xml", "xmls_tests/test3.xml", "xmls_tests/test4.xml");
-// $xmls = array("xmls/lea-ThinkPad-E570-2020-09-24-11-21-01.xml", "xmls/OCS-SRV-STAGE-LEA-2020-10-01-08-37-40.xml");
-
-
-// loop other xmls to get arrays as objects
-$i = 0;
-foreach ($xmls as $xml) {
-    $arr = new ArrayFromXML($xml);
-    $devices[$i]= $arr;
-    $i++;
+//show mac address on the tab
+$show_mac_addr = true;
+$tab_options = $protectedPost;
+$tab_options['form_name'] = "show_all";
+$form_name = $tab_options['form_name'];
+$tab_options['table_name'] = "list_show_all";
+if (isset($protectedGet['filtre']) && !isset($protectedPost['FILTRE'])) {
+    if (substr($protectedGet['filtre'], 0, 9) == "a.fields_") {
+        $values_accountinfo = accountinfo_tab(substr($protectedGet['filtre'], 9));
+        if (is_array($values_accountinfo)) {
+            $protectedPost['FILTRE_VALUE'] = $values_accountinfo[$protectedGet['value']];
+        }
+    }
+    $protectedPost['FILTRE'] = $protectedGet['filtre'];
+    if (!isset($protectedPost['FILTRE_VALUE'])) {
+        $protectedPost['FILTRE_VALUE'] = $protectedGet['value'];
+    }
 }
 
-
-// first array is main device to compare with
-$mainDevice =  array_shift($devices);
-$mainDevice = $mainDevice->array;
+// text 
+echo "<h2>Choose main device to use for comparison : </h2><br>";
 
 
-// loop over array of other devices to get differences
-foreach ($devices as $key => $value) {
-	$diffsV1 = new Difference();
-	$array = $value->array;
-	$diffs[$key] = $diffsV1->getDifferencesV1($mainDevice, $array);
-}
-// var_dump($diffs);
-
-
-// COMMENTS
-echo "<h2> First table below displays characteristics of main device used for comparison </h2>";
-echo "<p> Other tables only display differences between main device and other selected devices </p>";
-
-
-// show main device array
-$mainDeviceTable = new Tabletizer();
-echo $mainDeviceTable->fromArray($mainDevice);
-
-
-// create tables for each array of differences (?)
-foreach ($diffs as $array) {
-	$table = new Tabletizer();
-	echo $table->fromArray($array);
+//del the selection
+if ($protectedPost['DEL_ALL'] != '') {
+    foreach ($protectedPost as $key => $value) {
+        $checkbox = explode('check', $key);
+        if (isset($checkbox[1])) {
+            deleteDid($checkbox[1]);
+        }
+    }
+    $tab_options['CACHE'] = 'RESET';
 }
 
+//delete one computer
+if ($protectedPost['SUP_PROF'] != '') {
+    deleteDid($protectedPost['SUP_PROF']);
+    $tab_options['CACHE'] = 'RESET';
+}
 
-echo "<br><br><span> Array of differences : </span><br>";
-var_dump($diffs);
+if (!isset($protectedPost['tri_' . $table_name]) || $protectedPost['tri_' . $table_name] == "") {
+    $protectedPost['tri_' . $table_name] = "h.lastdate";
+    $protectedPost['sens_' . $table_name] = "DESC";
+}
+echo open_form($form_name, '', '', 'form-horizontal');
+//BEGIN SHOW ACCOUNTINFO
+$accountinfo_value = interprete_accountinfo($list_fields, $tab_options);
+if (array($accountinfo_value['TAB_OPTIONS'])) {
+    $tab_options = $accountinfo_value['TAB_OPTIONS'];
+}
+if (array($accountinfo_value['DEFAULT_VALUE'])) {
+    $default_fields = $accountinfo_value['DEFAULT_VALUE'];
+}
+$list_fields = $accountinfo_value['LIST_FIELDS'];
+//END SHOW ACCOUNTINFO
+$list_fields2 = array($l->g(46) => "h.lastdate",
+    'NAME' => 'h.name',
+    $l->g(949) => "h.ID",
+    $l->g(24) => "h.userid",
+    $l->g(25) => "h.osname",
+    $l->g(568) => "capa",
+    $l->g(569) => "h.processors",
+    $l->g(33) => "h.workgroup",
+    $l->g(275) => "h.osversion",
+    $l->g(286) => "h.oscomments",
+    $l->g(350) => "h.processort",
+    $l->g(351) => "h.processorn",
+    $l->g(50) => "h.swap",
+    $l->g(352) => "h.lastcome",
+    $l->g(353) => "h.quality",
+    $l->g(354) => "h.fidelity",
+    $l->g(53) => "h.description",
+    $l->g(355) => "h.wincompany",
+    $l->g(356) => "h.winowner",
+    $l->g(357) => "h.useragent",
+    $l->g(64) => "e.smanufacturer",
+    $l->g(284) => "e.bmanufacturer",
+    $l->g(36) => "e.ssn",
+    $l->g(65) => "e.smodel",
+    $l->g(209) => "e.bversion",
+    $l->g(34) => "ipaddress",
+    $l->g(557) => "h.userdomain",
+    $l->g(1247) => "h.ARCH",
+    $l->g(210) => "e.bdate",
+    $l->g(61) => "vname");
+if ($show_mac_addr) {
+    $list_fields2[$l->g(95)] = "n.macaddr";
+    $list_fields2[$l->g(208)] = "n.ipmask";
+    $list_fields2[$l->g(207)] = "n.ipgateway";
+    $list_fields2[$l->g(331)] = "n.ipsubnet";
+}
 
+$list_fields = array_merge($list_fields, $list_fields2);
+
+// Create select field
+$select_fields2 = array($l->g(46) => "h.lastdate",
+    'NAME' => 'h.name',
+    $l->g(949) => "h.ID",
+    $l->g(24) => "h.userid",
+    $l->g(25) => "h.osname",
+    $l->g(568) => "h.memory as capa",
+    $l->g(569) => "h.processors",
+    $l->g(33) => "h.workgroup",
+    $l->g(275) => "h.osversion",
+    $l->g(286) => "h.oscomments",
+    $l->g(350) => "h.processort",
+    $l->g(351) => "h.processorn",
+    $l->g(50) => "h.swap",
+    $l->g(352) => "h.lastcome",
+    $l->g(353) => "h.quality",
+    $l->g(354) => "h.fidelity",
+    $l->g(53) => "h.description",
+    $l->g(355) => "h.wincompany",
+    $l->g(356) => "h.winowner",
+    $l->g(357) => "h.useragent",
+    $l->g(64) => "e.smanufacturer",
+    $l->g(284) => "e.bmanufacturer",
+    $l->g(36) => "e.ssn",
+    $l->g(65) => "e.smodel",
+    $l->g(209) => "e.bversion",
+    $l->g(34) => "GROUP_CONCAT(DISTINCT n.ipaddress SEPARATOR 0x2D) as ipaddress",
+    $l->g(557) => "h.userdomain",
+    $l->g(1247) => "h.ARCH",
+    $l->g(210) => "e.bdate",
+    $l->g(61) => "v.name as vname");
+
+// List for select
+$select_fields = array_merge($list_fields, $select_fields2);
+
+$tab_options['FILTRE'] = array_flip($list_fields);
+$tab_options['FILTRE']['h.name'] = $l->g(23);
+asort($tab_options['FILTRE']);
+if ($_SESSION['OCS']['profile']->getConfigValue('DELETE_COMPUTERS') == "YES") {
+    $list_fields['CHECK'] = 'h.ID';
+    $list_fields['SUP'] = 'h.ID';
+}
+$list_col_cant_del = array('SUP' => 'SUP', 'NAME' => 'NAME', 'CHECK' => 'CHECK');
+$default_fields2 = array($_SESSION['OCS']['TAG_LBL']['TAG'] => $_SESSION['OCS']['TAG_LBL'], $l->g(46) => $l->g(46), 'NAME' => 'NAME', $l->g(23) => $l->g(23),
+    $l->g(24) => $l->g(24), $l->g(25) => $l->g(25), $l->g(568) => $l->g(568),
+    $l->g(569) => $l->g(569));
+$default_fields = array_merge($default_fields, $default_fields2);
+$sql = prepare_sql_tab($select_fields, array('SUP', 'CHECK'));
+$tab_options['ARG_SQL'] = $sql['ARG'];
+$queryDetails = $sql['SQL'] . " from hardware h
+				LEFT JOIN accountinfo a ON a.hardware_id=h.id  ";
+
+if ($show_mac_addr) {
+    $queryDetails .= "	LEFT JOIN networks n ON n.hardware_id=h.id ";
+}
+// BIOS INFOS
+$queryDetails .= "LEFT JOIN bios e ON e.hardware_id=h.id ";
+
+//MEMORIES INFOS
+$queryDetails .= "LEFT JOIN memories m ON m.hardware_id=h.id ";
+
+// VIDEOS CARDS INFOS
+$queryDetails .= "LEFT JOIN videos v ON v.hardware_id=h.id ";
+$queryDetails .= "where deviceid<>'_SYSTEMGROUP_' AND deviceid<>'_DOWNLOADGROUP_' ";
+// TAG RESTRICTIONS
+if (is_defined($_GET['value']) && $_GET['filtre'] == "a.TAG") {
+    $tag = $_GET['value'];
+    $queryDetails .= "AND a.TAG= '$tag' ";
+}
+
+// TAG RESTRICTIONS
+if (is_defined($_GET['value']) && strpos($_GET['filtre'], 'a.fields_') === 0){
+    $tag = $_GET['value'];
+    $fields = $_GET['filtre'];
+    $queryDetails .= "AND ".$fields."= '$tag' ";
+}
+
+if (is_defined($_SESSION['OCS']["mesmachines"])) {
+    $queryDetails .= "AND " . $_SESSION['OCS']["mesmachines"];
+}
+$queryDetails .= " group by h.id";
+$tab_options['LBL_POPUP']['SUP'] = 'name';
+$tab_options['LBL']['SUP'] = $l->g(122);
+$tab_options['TRI']['DATE']['e.bdate'] = "%m/%d/%Y"; // BIOS date format in database (varchar)
+
+$entete = ajaxtab_entete_fixe($list_fields, $default_fields, $tab_options, $list_col_cant_del);
+
+
+echo close_form();
+
+if (AJAX) {
+    ob_end_clean();
+    tab_req($list_fields, $default_fields, $list_col_cant_del, $queryDetails, $tab_options);
+    }
 ?>
