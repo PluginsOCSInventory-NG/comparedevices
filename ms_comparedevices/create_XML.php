0a764d537d12d57c2e25349393dbf3de2cb6a4bd
Charlène Auger <charlene.auger@factorfx.com>
2021-06-18 11:30:50 +0200

    Fix mysqli return false

--- ms_comparedevices/create_XML.php
+++ ms_comparedevices/create_XML.php
@@ -5,10 +5,15 @@ Returns a string
 */
 class DeviceXML {
     function createXML($id) {
-        $sql = "select * from hardware where id=%s";
+        $sql = "SELECT * FROM `hardware` where `ID`=%s";
         $arg = $id;
         $res = mysql2_query_secure($sql, $_SESSION['OCS']["readServer"], $arg);
-        $item_hardware = mysqli_fetch_object($res);
+        $item_hardware = null;
+
+        if($res) {
+            $item_hardware = mysqli_fetch_object($res);
+        }
+        
         $xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n";
         $table_not_use = array('accountinfo', 'groups_cache', 'download_history', 'devices');
         $xml .= "<REQUEST>\n";
@@ -16,25 +21,28 @@ class DeviceXML {
         $xml .= "\t<CONTENT>\n";
         foreach ($_SESSION['OCS']['SQL_TABLE_HARDWARE_ID'] as $tablename) {
             if (!in_array($tablename, $table_not_use)) {
-                $sql = "select * from %s where hardware_id=%s";
+                $sql = "SELECT * FROM `%s` WHERE `HARDWARE_ID`=%s";
                 $arg = array($tablename, $id);
 
                 $res = mysql2_query_secure($sql, $_SESSION['OCS']["readServer"], $arg);
-                while ($item = mysqli_fetch_object($res)) {
-                    $xml .= "\t\t<" . mb_strtoupper($tablename) . ">\n";
-                    foreach ($_SESSION['OCS']['SQL_TABLE'][$tablename] as $field_name => $field_type) {
-                        if ($field_name != 'HARDWARE_ID') {
-                            if (replace_entity_xml($item->$field_name) != '') {
-                                $xml .= "\t\t\t<" . $field_name . ">";
-                                $xml .= replace_entity_xml($item->$field_name);
-                                $xml .= "</" . $field_name . ">\n";
-                            } else {
-                                $xml .= "\t\t\t<" . $field_name . " />\n";
+
+                if($res) {
+                    while ($item = mysqli_fetch_object($res)) {
+                        $xml .= "\t\t<" . mb_strtoupper($tablename) . ">\n";
+                        foreach ($_SESSION['OCS']['SQL_TABLE'][$tablename] as $field_name => $field_type) {
+                            if ($field_name != 'HARDWARE_ID') {
+                                if (replace_entity_xml($item->$field_name) != '') {
+                                    $xml .= "\t\t\t<" . $field_name . ">";
+                                    $xml .= replace_entity_xml($item->$field_name);
+                                    $xml .= "</" . $field_name . ">\n";
+                                } else {
+                                    $xml .= "\t\t\t<" . $field_name . " />\n";
+                                }
                             }
                         }
+                        $xml .= "\t\t</" . mb_strtoupper($tablename) . ">\n";
                     }
-                    $xml .= "\t\t</" . mb_strtoupper($tablename) . ">\n";
-                }
+                } 
             }
         }
         //HARDWARE INFO
@@ -53,11 +61,15 @@ class DeviceXML {
         $xml .= "\t\t</HARDWARE>\n";
 
         //ACCOUNTINFO VALUES
-        $sql = "select * from accountinfo where hardware_id=%s";
+        $sql = "SELECT * FROM `accountinfo` WHERE `HARDWARE_ID`=%s";
         $arg = $id;
         $res = mysql2_query_secure($sql, $_SESSION['OCS']["readServer"], $arg);
-        $item_accountinfo = mysqli_fetch_object($res);
+        $item_accountinfo = null;
 
+        if($res) {
+            $item_accountinfo = mysqli_fetch_object($res);
+        }
+        
         foreach ($_SESSION['OCS']['SQL_TABLE']['accountinfo'] as $field_name => $field_type) {
             if ($field_name != 'HARDWARE_ID') {
                 $xml .= "\t\t<ACCOUNTINFO>\n";
